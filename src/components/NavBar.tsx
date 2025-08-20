59cffcb20fca091b7be3ef2149f9d5cf693413d8
biprotosh <biprotosh123@gmail.com>
2025-08-20 16:43:26 +0530

    Fixed some changes as per tailwind v4

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,7 +1,17 @@
+import headerLogo  from '../assets/images/header-logo.svg' 
+import hamburger from '../assets/icons/hamburger.svg';
 
 const NavBar = () => {
   return (
-    <div>NavBar</div>
+    <>
+    <header className='padding-x'>
+        <nav className=''>
+            <a href="/">
+                <img src={headerLogo} alt="logo" width={130} height={29} />
+            </a>
+        </nav>
+    </header>
+    </>
   )
 }
 
