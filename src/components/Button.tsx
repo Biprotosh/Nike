db59f9e458a6b092e2c140f1055d78a73f97145c
biprotosh <biprotosh123@gmail.com>
2025-08-22 11:56:56 +0530

    Implimented super quality section and fix bug in button component

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -4,7 +4,7 @@ const Button = ({label, icon}: TButtonProps) => {
   return (
     <button className='flex justify-center items-center gap-2 px-7 py-4  font-montserrat text-lg leading-none bg-coral-red rounded-full text-white'>
         {label}
-        <img src={icon} alt="arrow right icon" className="ml-2 rounded-full w-5 h-5"/>
+        {icon && <img src={icon} alt="arrow right icon" className="ml-2 rounded-full w-5 h-5"/>}
     </button>
   )
 }
