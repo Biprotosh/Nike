b3f6e580fe8622f7602796bab72b5ab34783d0d6
biprotosh <biprotosh123@gmail.com>
2025-08-25 15:45:52 +0530

    Fixed some ui for mobile devices

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -47,7 +47,7 @@ const Hero = () => {
                 </div>
             </div>
 
-            <div className="relative flex flex-1 justify-center items-center xl:min-h-screen max-xl:py-40 bg-primary bg-hero bg-cover bg-center">
+            <div className="relative sm:flex flex-1 justify-center items-center xl:min-h-screen max-xl:py-40 bg-primary bg-hero bg-cover bg-center hidden">
                 <img
                     src={bigShoeImg}
                     alt="Shoe collection"
--- src/sections/PopularProducts.tsx
+++ src/sections/PopularProducts.tsx
@@ -3,7 +3,7 @@ import { products } from "../constants"
 
 const PopularProducts = () => {
   return (
-    <section id="products" className="max-container max-sm:mt-12">
+    <section id="products" className="max-container ">
       <div className="flex flex-col gap-5 justify-start">
         <h2 className="text-4xl font-palanquin font-bold">Our <span className="text-coral-red">Popular</span> Products</h2>
         <p className="lg:max-w-lg mt-2 font-montserrat text-slate-gray">Experience top-notch quality and style with our sought-after selections. Discover a world of comfort, design and value</p>
