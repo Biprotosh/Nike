887671bbbb31960bb49d3eda790ab27a9ac695b0
biprotosh <biprotosh123@gmail.com>
2025-08-25 15:29:08 +0530

    Fixed mobile view for pupular porducts

--- src/components/PopularProductCard.tsx
+++ src/components/PopularProductCard.tsx
@@ -4,13 +4,13 @@ import type { TProduct } from '../types'
 const PopularProductCard = ({name, imgURL, price}:TProduct) => {
   return (
     <div className='flex flex-1 flex-col w-full max-sm:w-full'>
-        <img src={imgURL} alt={name} className='w-[280px] h-[280px]'/>
+        <img src={imgURL} alt={name} className='w-[150px] h-[150px] sm:w-[280px] sm:h-[280px]'/>
         <div className='mt-4 flex justify-start gap-2.5'>
-            <img src={star} alt="rating" width={24} height={24}/>
-            <p className='font-montserrat text-xl leading-normal text-slate-gray'>4.5</p>
+            <img src={star} alt="rating" width={16} height={16}/>
+            <p className='font-montserrat sm:text-xl leading-normal text-slate-gray'>4.5</p>
         </div>
-        <h3 className='mt-2 text-2xl leading-normal font-semibold font-palanquin'>{name}</h3>
-        <p className='mt-2 text-2xl text-coral-red font-montserrat leading-normal font-semibold'>{price}</p>
+        <h3 className='mt-2 text-xl sm:text-2xl leading-normal font-semibold font-palanquin'>{name}</h3>
+        <p className='mt-2 text-xl sm:text-2xl text-coral-red font-montserrat leading-normal font-semibold'>{price}</p>
 
     </div>
   )
--- src/sections/PopularProducts.tsx
+++ src/sections/PopularProducts.tsx
@@ -9,7 +9,7 @@ const PopularProducts = () => {
         <p className="lg:max-w-lg mt-2 font-montserrat text-slate-gray">Experience top-notch quality and style with our sought-after selections. Discover a world of comfort, design and value</p>
       </div>
 
-      <div className="mt-16 grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 sm:gap-4 gap-14">
+      <div className="mt-16 grid lg:grid-cols-4 md:grid-cols-3 grid-cols-2 sm:gap-4 gap-8">
         {products.map((product) => (
           <PopularProductCard key={product.name} {...product}/>
         ))}
